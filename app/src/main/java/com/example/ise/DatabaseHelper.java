8add4148151c09e74316cfce23c648704e4263ac
Sharathkumar K G <54583811+kgsharathkumar@users.noreply.github.com>
2020-06-02 21:31:01 +0530

    updated Helper class

--- app/src/main/java/com/example/ise/DatabaseHelper.java
+++ app/src/main/java/com/example/ise/DatabaseHelper.java
@@ -8,66 +8,77 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DatabaseHelper extends SQLiteOpenHelper
-{
-    public static final String DATABSE_NAME="register.db";
-    public static final String TABLE_NAME="registeruser";
-    public static final String COL_1="ID";
-    public static final String COL_2="username";
-    public static final String COL_3="password";
+public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(@Nullable Context context)
-    {
-        super(context, DATABSE_NAME, null, 1);
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "Mydatabase.db";
+
+    // Contacts table name
+    private static final String TABLE_REGISTER= "register";
+    private static final String DATABASE_PATH = "/data/data/com.example.ise/databases/";
+    // public static final String TABLE_NAME="registeruser";
+    public static final String KEY_ID = "ID";
+    public static final String KEY_USER_NAME = "username";
+    public static final String KEY_PASSWORD = "password";
+
+    public static final String CREATE_TABLE="CREATE TABLE " + TABLE_REGISTER + "("
+            + KEY_ID + " INTEGER PRIMARY KEY," + KEY_USER_NAME + " TEXT," + KEY_PASSWORD + " TEXT " + ")";
+
+    public DatabaseHelper(Context context, Object name,
+                          Object factory, int version) {
+        // TODO Auto-generated constructor stub
+        super(context,  DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+
 
     @Override
-    public void onCreate(SQLiteDatabase  SQLiteDatabase )
-    {
-        SQLiteDatabase.execSQL("CREATE TABLE registeruser (ID INTEGER PRIMARY KEY AUTOINCREMENT,username Text,passwod Text)");
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(CREATE_TABLE);
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase SQLiteDatabase , int i , int i1)
-    {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_REGISTER);
 
-        SQLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-
-        onCreate(SQLiteDatabase);
-
+        // Create tables again
+        onCreate(db);
     }
 
-    public  long adduser(String user,String password)
+
+    public void addregister(RegisterData registerdata)
     {
       SQLiteDatabase db =this.getWritableDatabase();
-        ContentValues contentValues=new ContentValues();
-        contentValues.put("username",user);
-        contentValues.put("password",password);
-        long res= db.insert("registeruser",null,contentValues);
-        db.close();
-        return res;
+        ContentValues values = new ContentValues();
+        values.put(KEY_USER_NAME,registerdata.getUser_name()); // register first Name
+        values.put(KEY_PASSWORD, registerdata.getPassword() ); // register last name
+
+        // Inserting Row
+        db.insert(TABLE_REGISTER, null, values);
+        db.close(); // Closing database connection
 
     }
+    String password;
+    //code to get the register
+    String getregister(String username){
+        SQLiteDatabase db = this.getReadableDatabase();
+        //String selectquery="SELECT * FROM TABLE_REGISTER";
+        Cursor cursor=db.query(TABLE_REGISTER,null,  "password=?",new String[]{username},null, null, null, null);
 
-    public boolean checkuser(String username, String password)
-    {
-        String[] columns= {COL_1};
-        SQLiteDatabase db =getReadableDatabase();
-        String selection=COL_2 +  "=?" + " and " +  COL_3 + "=?";
-        String[] selectionargs={ username,password};
-        Cursor cursor=db.query(TABLE_NAME,columns,selection,selectionargs,null,null,null);
-        int count= cursor.getCount();
-        cursor.close();
-        db.close();
+        if(cursor.getCount()<1){
+            cursor.close();
+            return "Not Exist";
+        }
+        else if(cursor.getCount()>=1 && cursor.moveToFirst()){
 
-        if(count>0)
+            password = cursor.getString(cursor.getColumnIndex(KEY_PASSWORD));
+            cursor.close();
 
-                return true;
-        else
-                return false;
-
+        }
+        return password;
     }
-
 }
-
-
